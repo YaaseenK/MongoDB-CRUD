d9ceaca51d8d91728f018e38839eb221f2993b5c
yaaseenk <yaaseenkhan02@gmail.com>
2025-01-26 20:44:07 -0500

    server and db are connected and server is giving respones.

--- 01Strarter/index.js
+++ 01Strarter/index.js
@@ -2,4 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const URI = "mongodb://localhost:27017/NodeAPI"
 
+app.get('/', (req, res) => {
+    res.send("Hello from node APIs Updated!!")
+});
+
+
+mongoose.connect(URI)
+.then(() => {
+    console.log("Connected to DB");
+    app.listen(3001 , () => {
+        console.log('Server is Running on port 3001')
+    });    
+})
+
+.catch(()=> {
+    console.log("Connection failed")
+});

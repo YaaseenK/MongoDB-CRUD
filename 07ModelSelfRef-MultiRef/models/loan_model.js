029cdf0c3a924c379afdbde9b62757791d5f3b19
yaaseenk <yaaseenkhan02@gmail.com>
2025-01-29 11:19:46 -0500

    added a loan model

--- 07ModelSelfRef-MultiRef/models/loan_model.js
+++ 07ModelSelfRef-MultiRef/models/loan_model.js
@@ -1,1 +1,33 @@
-const  { Schema, Types, model } = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
+
+const LoanSchema = new Schema({
+    user: {
+        type: Types.ObjectId, 
+        ref: 'User',
+        required: true},
+    book: {
+        type:Types.ObjectId,
+        ref: 'Book',
+        required: true
+    },
+    borrowedDate:{
+        type: Date,
+        default: Date.now
+    },
+    dueDate:{
+        type: Date,
+        required: true
+    },
+    returnDate:{
+        type: Date,
+    },
+    status:{
+        type: String,
+        enum: ['borrowed', 'returned'],
+        default: 'borrowed',
+    },
+},
+    {timestamps:true}
+);
+
+module.exports = model('Loan', LoanSchema);
